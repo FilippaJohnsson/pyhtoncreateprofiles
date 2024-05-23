4c5d7090e4ecac8ec2bfa44435f554626a52e3e8
FilippaJohnsson <Filippa.Johnsson1986@gmail.com>
2024-05-24 01:30:20 +0200

    To make workflow build properly when all tests pass

--- WorkflowTests.py
+++ WorkflowTests.py
@@ -3,7 +3,7 @@ import unittest
 class WorkflowTests(unittest.TestCase):
           
     def test_workflow_should_always_pass(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()

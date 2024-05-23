d783a85c6d9364ec2c401a5f8ec3c43bfaa7252e
FilippaJohnsson <Filippa.Johnsson1986@gmail.com>
2024-05-24 01:27:03 +0200

    Temp commit to test workflow break if a unit test fails

---
+++ WorkflowTests.py
@@ -0,0 +1,9 @@
+import unittest
+
+class WorkflowTests(unittest.TestCase):
+          
+    def test_workflow_should_always_pass(self):
+        self.assertTrue(False)
+
+if __name__ == '__main__':
+    unittest.main()
